b8b9128a57198ede43604f1f13b524cc65bea23a
WHtheGetHard <nakashima.hi5@gmail.com>
2019-03-04 13:23:22 +0900

    create exam result registration function from display.

--- src/player/ExamResultRegist.java
+++ src/player/ExamResultRegist.java
@@ -3,14 +3,21 @@ package player;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fieldformat.DailyExamResult;
 import fieldformat.ExamResult;
+import fieldformat.ResultDetail;
+import fieldformat.ShowMessage;
 import flayer.ExamResultEvaluating;
+import flayer.ExecInsertDailyExamResult;
+import flayer.ExecInsertResultDetail;
+import flayer.ExecSelectResultDetail;
 
 /**
  * Servlet implementation class ExamResultRegist
@@ -44,6 +51,63 @@ public class ExamResultRegist extends HttpServlet {
 		ExamResultEvaluating examResultEvaluating = new ExamResultEvaluating();
 		examResultList = examResultEvaluating.execEvaluating(examResultList);
 
+		String yyyymmdd = examResultList.get(0).getExamDate();
+		String accuracy = examResultList.get(0).getValidity();
+
+
+		ExecSelectResultDetail execSelectResultDetail = new ExecSelectResultDetail();
+		execSelectResultDetail.execSelectMaxId();
+
+		ShowMessage showMessage = new ShowMessage();
+		showMessage = execSelectResultDetail.getShowMessage();
+
+		if(showMessage.isAbEnd()) {
+			request.setAttribute("showMessage", showMessage);
+
+			RequestDispatcher rd = request.getRequestDispatcher("Exam.jsp");
+
+			rd.forward(request, response);
+
+			return;
+		}
+
+		ResultDetail resultDetail = new ResultDetail();
+		resultDetail.setId(execSelectResultDetail.getMaxId() + 1);
+		resultDetail.setYyyymmdd(yyyymmdd);
+		resultDetail.setAccuracy(accuracy);
+
+		ExecInsertResultDetail execInsertResultDetail = new ExecInsertResultDetail();
+		showMessage = execInsertResultDetail.execInsertResultDetail(resultDetail);
+
+		if(showMessage.isAbEnd()) {
+			request.setAttribute("showMessage", showMessage);
+
+			RequestDispatcher rd = request.getRequestDispatcher("Exam.jsp");
+
+			rd.forward(request, response);
+
+			return;
+		}
+
+		ArrayList<DailyExamResult> dailyExamResultList = new ArrayList<DailyExamResult>();
+		for(int i = 0; i < 10; i++) {
+			DailyExamResult dailyExamResult = new DailyExamResult();
+			dailyExamResult.setYyyymmdd(yyyymmdd);
+			dailyExamResult.setDetailid(resultDetail.getId());
+			dailyExamResult.setWordid(examResultList.get(i).getWordId());
+			dailyExamResult.setResult(examResultList.get(i).getResult());
+
+			dailyExamResultList.add(dailyExamResult);
+		}
+
+		ExecInsertDailyExamResult execInsertDailyExamResult = new ExecInsertDailyExamResult();
+		showMessage = execInsertDailyExamResult.execInsertDailyExamResult(dailyExamResultList);
+
+		request.setAttribute("showMessage", showMessage);
+
+		RequestDispatcher rd = request.getRequestDispatcher("Exam.jsp");
+
+		rd.forward(request, response);
 
 	}
 
